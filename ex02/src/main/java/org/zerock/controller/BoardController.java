9dd7e790f13f2b1bff0311f33c3aa191929bec54
hyejin0810 <heajin0810@naver.com>
2022-12-17 22:02:24 +0900

    part3 p224

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,50 @@
+package org.zerock.controller;
+
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import sun.reflect.generics.visitor.Reifier;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+@AllArgsConstructor//controller 가 service에 대해서 의존적으로 생성자를 만들고 주입함
+public class BoardController {
+	private BoardService service;
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAllAttributes(service.getList());
+	}
+	@PostMapping("/register")
+	public String register(BoardVO board, RedirectAttributes rttr) {
+		log.info("register:"+board);
+		service.register(board);
+		rttr.addFlashAttribute("result", board.getBno());
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/get")
+	public void get(@RequestParam("bno") Long bno, Model model) {
+		log.info("/get");
+		model.addAttribute("board", service.get(bno));
+	}
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify:"+board);
+		if(service.modify(board)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:board/list";
+	}
+}
